08f5e64a5f41e35c8432e12ec719c327e8eb333b
NI54-oficina <frantrf@gmail.com>
2016-03-15 19:02:40 -0300

    19:02 15-03 coni
    paises

--- protected/views/static/paises.php
+++ protected/views/static/paises.php
@@ -4,14 +4,38 @@
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="bower_components/BigVideo/css/bigvideo.css">
     <script src="bower_components/modernizr/modernizr-2.5.3.min.js"></script>
+
+<style>
+.a-paises-principal{
+	color:white; 
+	font-family:'arialregular';
+}
+
+.a-paises-principal img{
+	display:block;
+	margin:auto;
 	
+}
+
+.a-paises-principal p{
+	text-align:center;
+	line-height: 25px;
+	font-weight:700;
+	font-size:15px;
+	letter-spacing:1px;
+}
+
+.banderas-paises-principal{}
+
+</style>
+
 </head>
    <!-- BigVideo Dependencies -->
-   
     <script src="bower_components/jquery-ui/ui/jquery-ui.js"></script>
     <script src="bower_components/jquery-ui/ui/minified/jquery-ui.min.js"></script>
     <script src="bower_components/video.js"></script>
 
+  <div style="width:100vw; height:100vh; z-index:10; position:relative;background-color:rgba(0,0,0,0.5)"> 
     <!-- BigVideo -->
     <script src="bower_components/BigVideo/lib/bigvideo.js"></script>
 
@@ -24,3 +48,27 @@
 		BV.show('video/home.mp4',{ambient:true});
 	    });
     </script>
+	<div class="col-lg-1"></div>
+	<!-- PAÍSES -->
+	<div id="paises-home" class="col-lg-10 col-md-10 " style="color:white;margin-top:20vh;">
+	
+			<div class="col-lg-3 col-md-3 banderas-paises-principal"><a href="" class="a-paises-principal"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/band-arg.png" /><p>Argentina</p></a></div>
+			<div class="col-lg-3 col-md-3 banderas-paises-principal"><a href="" class="a-paises-principal"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/band-bol.png" /><p>Bolivia</p></a></div>
+			<div class="col-lg-3 col-md-3 banderas-paises-principal"><a href="" class="a-paises-principal"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/band-br.png" /><p>Brasil</p></a></div>
+			<div class="col-lg-3 col-md-3 banderas-paises-principal"><a href="" class="a-paises-principal"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/band-cost.png" /><p>Costa Rica</p></a></div>
+			<div class="col-lg-3 col-md-3 banderas-paises-principal"><a href="" class="a-paises-principal"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/band-ec.png" /><p>Ecuador</p></a></div>
+			<div class="col-lg-3 col-md-3 banderas-paises-principal"><a href="" class="a-paises-principal"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/band-sal.png" /><p>El Salvador</p></a></div>
+			<div class="col-lg-3 col-md-3 banderas-paises-principal"><a href="" class="a-paises-principal"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/band-guat.png" /><p>Guatemala</p></a></div>
+			<div class="col-lg-3 col-md-3 banderas-paises-principal"><a href="" class="a-paises-principal"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/band-hon.png" /><p>Honduras</p></a></div>
+			<div class="col-lg-3 col-md-3 banderas-paises-principal"><a href="" class="a-paises-principal"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/band-mex.png" /><p>México</p></a></div>
+			<div class="col-lg-3 col-md-3 banderas-paises-principal"><a href="" class="a-paises-principal"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/band-nic.png" /><p>Nicaragua</p></a></div>
+			<div class="col-lg-3 col-md-3 banderas-paises-principal"><a href="" class="a-paises-principal"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/band-pan.png" /><p>Panamá</p></a></div>
+			<div class="col-lg-3 col-md-3 banderas-paises-principal"><a href="" class="a-paises-principal"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/band-par.png" /><p>Paraguay</p></a></div>
+			<div class="col-lg-3 col-md-3 banderas-paises-principal"><a href="" class="a-paises-principal"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/band-per.png" /><p>Perú</p></a></div>
+			<div class="col-lg-3 col-md-3 banderas-paises-principal"><a href="" class="a-paises-principal"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/band-rep.png" /><p>Rep. Dominicana</p></a></div>
+			<div class="col-lg-3 col-md-3 banderas-paises-principal"><a href="" class="a-paises-principal"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/band-uru.png" /><p>Uruguay</p></a></div>
+			<div class="col-lg-3 col-md-3 banderas-paises-principal"><a href="" class="a-paises-principal"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/band-ven.png" /><p>Venezuela</p></a></div>
+	
+	</div>
+
+</div>	
