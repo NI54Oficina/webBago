259c92fad62af735ad91c1822ef40ab39c88bb5b
melmiranda <melania.smc@hotmail.com>
2016-03-16 13:19:43 -0300

    13:19 pm 16-03-2016 Melania
    Nuestras-plantas

--- protected/views/static/nuestras-plantas.php
+++ protected/views/static/nuestras-plantas.php
@@ -1,6 +1,6 @@
 <img id="logo-bago-mobile" class="hidden-lg hidden-md col-sm-12 col-xs-12" src="/webBago/img/logo-bago.png" style="opacity:0;">
 			<div class="header-layout-aftosa col-lg-12 col-md-12 col-sm-12 col-xs-12">
-				<div style="width:100%;position:absolute;"><h1 class="titulo-layout-aftosa" style="">Nuestras plantas</h1></div>
+				<div style="width:100%;position:absolute;"><h1 class="titulo-layout-aftosa" style=""><?php echo Textos::model()->GetText(14); ?></h1></div>
 				<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/header-plantas.png" style="width:100vw;" />	
 			</div>
 	
@@ -18,7 +18,7 @@
 	<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
 	<div class="side-title">
 		<div style="display:inline-block;">
-		<h2  class="h2-aftosa">Institucional</h2>	
+		<h2  class="h2-aftosa"><?php echo Textos::model()->GetText(347); ?></h2>	
 		<div style="width:50%;height:1px;display:inline-block;border-top:1px solid black;"></div>
 		</div>
 	</div>
@@ -29,77 +29,77 @@
 
 	<div class="tabla-aftosa col-lg-12 col-md-12 col-sm-12 col-xs-12">
 		<div  class="tr  col-lg-12 col-md-12 col-sm-12 col-xs-12 inner-text" style="text-shadow:none;padding-top:0;">		
-					<p><strong>Nuestras dos plantas modelo de producción en Argentina y una planta en Brasil, equipadas con última tecnología, nos permiten ofrecer las más avanzadas soluciones para la salud animal.</strong></p>
-					<p><strong>Trabajamos para abastecer a los mercados con los más altos estándares de calidad. La excelencia de nuestros productos comienza con la selección y control de las mejores materias primas y se sostiene en una gestión de calidad integrada que garantiza 	la consistencia en cada serie. Las plantas son auditadas por las entidades regulatorias más exigentes del mundo. Todos nuestros procesos se desarrollan bajos las normas ISO 9001, 14.001 y de Buenas Prácticas de Manufacturación (GMP)</strong></p>
+					<p><strong><?php echo Textos::model()->GetText(77); ?></strong></p>
+					<p><strong><?php echo Textos::model()->GetText(78); ?></strong></p>
 				
-					<h2 style="margin-top:10%;color:#00255B;">Planta Garín, Buenos Aires, Argentina: </h2>
-					<h3 class="h3-plantas">Vacunas más consistentes y efectivas</h3>
-					<p>Construimos una planta modelo, única por su tecnología y capacidad de producción. Con introducción de equipamiento de última generación, evolucionamos en nuevos sistemas de  producción, concentración y purificación de antígenos que, combinados con adyuvantes específicos, nos permiten formular las vacunas más potentes y eficaces. <br />
-					Además de los métodos convencionales desarrollamos y patentamos una nueva tecnología de cultivo celular y de producción viral, para asegurar mayor calidad y  cantidad de antígenos en cada una de nuestras vacunas.
+					<h2 style="margin-top:10%;color:#00255B;"><?php echo Textos::model()->GetText(79); ?> </h2>
+					<h3 class="h3-plantas"><?php echo Textos::model()->GetText(80); ?></h3>
+					<p><?php echo Textos::model()->GetText(81); ?> <br />
+					<?php echo Textos::model()->GetText(82); ?>
 					</p>
 				
-					<p style="margin-top:3%;">Además de los métodos convencionales desarrollamos y patentamos una nueva tecnología de cultivo celular y de producción viral, para asegurar mayor calidad y cantidad de antígenos en cada una de nuestras vacunas.</p>
+				
 					
 					<ul class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="margin-top:3%;">
 					
 						<li class="col-lg-12 col-md-12 col-sm-12 col-xs-12 li-plantas "><img class="icono-bullet col-lg-2 col-md-2 col-sm-2 col-xs-2" src="img/icono-bullet.png"  />
-							<strong>Unidad de producción de Vacuna contra la Fiebre Aftosa: </strong>200 millones de dosis anuales, bajo las más altas  normas de bioseguridad, nivel 4 (NBS 4 OIE).
+							<?php echo Textos::model()->GetText(83); ?>
 						</li>
 					
 						<li class="col-lg-12 col-md-12 col-sm-12 col-xs-12 li-plantas " style="margin-top:5%;"><img class="icono-bullet col-lg-2 col-md-2 col-sm-2 col-xs-2" src="img/icono-bullet.png"  />
-							<strong>Unidad de producción de Biológicos ViBa:</strong> 100 millones de dosis anuales de vacuna combinada, destinadas a la prevención de enfermedades virales y bacterianas, bajo niveles de Bioseguridad 2 y 2+. 
+							<?php echo Textos::model()->GetText(84); ?>
 						</li>
 					
 						<li class="col-lg-12 col-md-12 col-sm-12 col-xs-12 li-plantas " style="margin-top:5%;"><img class="icono-bullet col-lg-2 col-md-2 col-sm-2 col-xs-2" src="img/icono-bullet.png"  />
-							<strong>Unidad de producción de Vacuna Antirrábica para bovinos, caninos y felinos:  </strong> 30 millones de dosis anuales,  elaboradas bajo niveles de bioseguridad 3 y  normas específicas de la OIE.
+							<?php echo Textos::model()->GetText(85); ?>
 						</li>
 					</ul>
 
 					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-						<h2 style="margin-top:10%;color:#00255B">Planta Monte Grande- Buenos Aires, Argentina</h2>
+						<h2 style="margin-top:10%;color:#00255B"><?php echo Textos::model()->GetText(86); ?></h2>
 				
-							<h3 class="h3-plantas">Soluciones para los principales problemas de la producción ganadera</h3>
+							<h3 class="h3-plantas"><?php echo Textos::model()->GetText(87); ?></h3>
 				
-								<p>Aquí elaboramos nuestra línea de productos farmacéuticos para ofrecer planes sanitarios completos y eficaces, adaptados a las necesidades de la producción ganadera.</p>
+								<p><?php echo Textos::model()->GetText(88); ?></p>
 				
-								<p>La planta cuenta con  <strong>6 unidades de producción</strong>, con una capacidad anual  <strong>de 3 millones de unidades al año:</strong></p>
+								<p><?php echo Textos::model()->GetText(89); ?></p>
 				
 							<ul class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="margin-top:3%;">
 					
-								<li class="col-lg-12 col-md-12 col-sm-12 col-xs-12 li-plantas"><img src="img/icono-bullet.png"  class="icono-bullet"/>Línea de Inyectables</li>
+								<li class="col-lg-12 col-md-12 col-sm-12 col-xs-12 li-plantas"><img src="img/icono-bullet.png"  class="icono-bullet"/>c</li>
 					
-								<li class="col-lg-12 col-md-12 col-sm-12 col-xs-12 li-plantas" style="margin-top:5%;"><img class="icono-bullet" src="img/icono-bullet.png"  />Línea de orales</li>
+								<li class="col-lg-12 col-md-12 col-sm-12 col-xs-12 li-plantas" style="margin-top:5%;"><img class="icono-bullet" src="img/icono-bullet.png"  /><?php echo Textos::model()->GetText(196); ?>91</li>
 					
-								<li class="col-lg-12 col-md-12 col-sm-12 col-xs-12 li-plantas" style="margin-top:5%;"><img class="icono-bullet" src="img/icono-bullet.png"  />Antibióticos (β – lactámicos)</li>
+								<li class="col-lg-12 col-md-12 col-sm-12 col-xs-12 li-plantas" style="margin-top:5%;"><img class="icono-bullet" src="img/icono-bullet.png"  /><?php echo Textos::model()->GetText(92); ?></li>
 					
-								<li class="col-lg-12 col-md-12 col-sm-12 col-xs-12  li-plantas" style="margin-top:5%;"><img class="icono-bullet" src="img/icono-bullet.png"  />Hormonales: Inyectables</li>
+								<li class="col-lg-12 col-md-12 col-sm-12 col-xs-12  li-plantas" style="margin-top:5%;"><img class="icono-bullet" src="img/icono-bullet.png"  /><?php echo Textos::model()->GetText(93); ?></li>
 					
-								<li class="col-lg-12 col-md-12 col-sm-12 col-xs-12  li-plantas" style="margin-top:5%;"><img class="icono-bullet" src="img/icono-bullet.png"  />Hormonales: sólidos</li>
+								<li class="col-lg-12 col-md-12 col-sm-12 col-xs-12  li-plantas" style="margin-top:5%;"><img class="icono-bullet" src="img/icono-bullet.png"  /><?php echo Textos::model()->GetText(94); ?></li>
 					
-								<li class="col-lg-12 col-md-12 col-sm-12 col-xs-12  li-plantas" style="margin-top:5%;"><img class="icono-bullet" src="img/icono-bullet.png"  />Ectoparasiticidas</li>
+								<li class="col-lg-12 col-md-12 col-sm-12 col-xs-12  li-plantas" style="margin-top:5%;"><img class="icono-bullet" src="img/icono-bullet.png"  /><?php echo Textos::model()->GetText(95); ?></li>
 
 							</ul>
                                   
 					</div>
                     
                     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-						<h2 style="margin-top:10%;color:#00255B;">Planta Arraçoiaba da Serra , Sao Paulo, Brasil</h2>
+						<h2 style="margin-top:10%;color:#00255B;"><?php echo Textos::model()->GetText(196); ?></h2>
 				
-							<h3 class="h3-plantas">Mas control sobre ectoparasitosis </h3>
+							<h3 class="h3-plantas"><?php echo Textos::model()->GetText(95); ?> </h3>
 				
-								<p>Para brindar soluciones tecnológicas que cubran las necesidades específicas de la ganadería en Brasil, instalamos una planta dedicada a la producción de ectoparaciticidas.<BR />
-									Nuestras instalaciones cumplen con los máximos estándares de calidad y con las normas de BPM (Buenas Prácticas de Manufactura) exigidas por el Ministerio de Agricultura, Pecuaria y Abastecimiento de Brasil. 
+								<p><?php echo Textos::model()->GetText(98); ?><BR />
+									<?php echo Textos::model()->GetText(99); ?> 
 								</p>
                  
 					</div>
                     
                     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-						<h2 style="margin-top:10%;color:#00255B;">Planta Jinhai Biotechnology, provincia de Shaanxi, China</h2>
+						<h2 style="margin-top:10%;color:#00255B;"><?php echo Textos::model()->GetText(100); ?></h2>
 				
-							<h3 class="h3-plantas"> Alta calidad de vacuna contra la Aftosa para la producción china </h3>
+							<h3 class="h3-plantas"><?php echo Textos::model()->GetText(101); ?></h3>
 				
-								<p>Jinhai Biotechnology es una empresa que surge del Joint Venture entre Biogénesis Bagó y  la compañía china Hile Biotechnology para la producción de vacuna contra la Fiebre Aftosa.
-								Con una superficie cubierta de casi 15000 m2 cubiertos, con tecnología de última generación, en la planta funcionan las áreas de producción,  los laboratorios de control de calidad, junto con los depósitos de materias primas y producto terminado. De acuerdo a lo requerido por la legislación china, la planta cuenta también con un sector segregado, con áreas de bioseguridad y acceso restringido, donde se realizan ensayos de control de calidad en animales.
+								<p><?php echo Textos::model()->GetText(102); ?>
+								<?php echo Textos::model()->GetText(103); ?>
 								</p>
                                
 					</div>
