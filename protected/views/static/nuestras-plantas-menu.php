a4193fcb53ce10305e6b03d852882772b7815eb6
NI54-oficina <frantrf@gmail.com>
2016-03-16 15:00:14 -0300

    15.00 pm 16-03 Coni
    Nuestras plantas 360

--- protected/views/static/nuestras-plantas-menu.php
+++ protected/views/static/nuestras-plantas-menu.php
@@ -3,52 +3,45 @@
 				<div style="width:100%;position:absolute;"><h1 class="titulo-layout-aftosa" style=""><?php echo Textos::model()->GetText(14); ?></h1></div>
 				<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/header-plantas.png" style="width:100vw;" />	
 			</div>
-	
-	<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-	<div class="side-title">
-		<div style="display:inline-block;">
-		<h2  class="h2-aftosa">Institucional</h2>	
-		<div style="width:50%;height:1px;display:inline-block;border-top:1px solid black;"></div>
-		</div>
-	</div>
-	</div>
 
 	
-<div class="div-texto-aftosa col-lg-5 col-md-5 col-sm-12 col-xs-12" style="text-align: left;">
+<div class="div-texto-aftosa col-lg-7 col-md-7 col-sm-12 col-xs-12 col-lg-offset-1" style="text-align: left;">
 
 	<div class="tabla-aftosa col-lg-12 col-md-12 col-sm-12 col-xs-12">
 	
 	<div  class="tr  col-lg-12 col-md-12 col-sm-12 col-xs-12 inner-text" style="text-shadow:none;padding-top:0;margin-bottom:3%;">	
 		
-		<h2 class="h2-aftosa" style="text-decoration:none; font-weight:700; font-size:25px;">Tour Biogenesis 360 °</h2>
+		<h2 class="h2-aftosa" style="text-decoration:none; font-weight:700; font-size:25px; margin-bottom: 27px;margin-top: 0;width:100%">Tour Biogenesis 360 °</h2>
+		
+		
 	
-		<div  class="tr  col-lg-12 col-md-12 col-sm-12 col-xs-12 inner-text" style="text-shadow:none;padding-top:0;margin-bottom:3%;">	
+		<div  class="  col-lg-6 col-md-6 col-sm-12 col-xs-12 inner-text container-lista-360" style="text-shadow:none;padding-top:0;margin-bottom:2.5%;">	
 			<a href="<?php echo Yii::app()->request->baseUrl; ?>/1080/Exterior/Exterior_1080.html" target="_blank">
-				<img  class="col-lg-12 col-md-12 col-sm-12 col-xs-12" src="img/imagen-360-exterior.png"  />
+				<img  src="img/imagen-360-exterior.png"  />
 			</a>
 		</div>
 	
-		<div  class="tr  col-lg-12 col-md-12 col-sm-12 col-xs-12 inner-text" style="text-shadow:none;padding-top:0;margin-bottom:3%;">	
+		<div  class="col-lg-6 col-md-6 col-sm-12 col-xs-12 inner-text container-lista-360" style="text-shadow:none;padding-top:0;margin-bottom:2.5%;">	
 			<a href="<?php echo Yii::app()->request->baseUrl; ?>/1080/Camara Frig/Camara_1080.html" target="_blank">
-				<img  class="col-lg-12 col-md-12 col-sm-12 col-xs-12" src="img/imagen-360-camara.png"  />
+				<img   src="img/imagen-360-camara.png"  />
 			</a>
 		</div>
 
-		<div  class="tr  col-lg-12 col-md-12 col-sm-12 col-xs-12 inner-text" style="text-shadow:none;padding-top:0;margin-bottom:3%;">	
+		<div  class="col-lg-6 col-md-6 col-sm-12 col-xs-12 inner-text container-lista-360" style="text-shadow:none;padding-top:0;margin-bottom:2.5%;">	
 			<a href="<?php echo Yii::app()->request->baseUrl; ?>/1080/Empaque/Empaque_10800.html" target="_blank">
-				<img  class="col-lg-12 col-md-12 col-sm-12 col-xs-12" src="img/imagen-360-empaque.png"  />
+				<img  src="img/imagen-360-empaque.png"  />
 			</a>
 		</div>
 
-		<div  class="tr  col-lg-12 col-md-12 col-sm-12 col-xs-12 inner-text" style="text-shadow:none;padding-top:0;margin-bottom:3%;">	
+		<div  class="col-lg-6 col-md-6 col-sm-12 col-xs-12 inner-text container-lista-360" style="text-shadow:none;padding-top:0;margin-bottom:2.5%;">	
 			<a href="<?php echo Yii::app()->request->baseUrl; ?>/1080/Sala Big Tubes/Sala_Big_Tubes_1080.html" target="_blank">
-				<img  class="col-lg-12 col-md-12 col-sm-12 col-xs-12" src="img/imagen-360-sala.png"  />
+				<img  src="img/imagen-360-sala.png"  />
 			</a>
 		</div>
 
-		<div  class="tr  col-lg-12 col-md-12 col-sm-12 col-xs-12 inner-text" style="text-shadow:none;padding-top:0;margin-bottom:3%;">	
+		<div  class="col-lg-6 col-md-6 col-sm-12 col-xs-12 inner-text container-lista-360" style="text-shadow:none;padding-top:0;margin-bottom:2.5%;">	
 			<a href="<?php echo Yii::app()->request->baseUrl; ?>/1080/Pasillo/Pasillo_1080.html" target="_blank">
-				<img  class="col-lg-12 col-md-12 col-sm-12 col-xs-12" src="img/imagen-360-pasillo.png"  />
+				<img  src="img/imagen-360-pasillo.png"  />
 			</a>
 		</div>		
 		</div>
