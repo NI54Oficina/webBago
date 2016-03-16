0b4ae0061c54f5bed12f7a62151c07b375a99bb5
melmiranda <melania.smc@hotmail.com>
2016-03-16 13:35:30 -0300

    13:35 16-03-2016 Mel
    mision-vision

--- protected/views/static/mision-vision.php
+++ protected/views/static/mision-vision.php
@@ -1,6 +1,6 @@
 <img id="logo-bago-mobile" class="hidden-lg hidden-md col-sm-12 col-xs-12" src="/webBago/img/logo-bago.png" style="opacity:0;">
 			<div class="header-layout-aftosa col-lg-12 col-md-12 col-sm-12 col-xs-12">
-				<div style="width:100%;position:absolute;"><h1 class="titulo-layout-aftosa" style="">Misión, visión y valores</h1></div>
+				<div style="width:100%;position:absolute;"><h1 class="titulo-layout-aftosa" style=""><?php echo Textos::model()->GetText(8); ?></h1></div>
 				<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/valores.png" style="width:100vw;" />	
 			</div>
 	</section>
@@ -9,7 +9,7 @@
 	<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
 	<div class="side-title">
 		<div style="display:inline-block;">
-		<h2  class="h2-aftosa">Institucional</h2>	
+		<h2  class="h2-aftosa"><?php echo Textos::model()->GetText(347); ?></h2>	
 		<div style="width:50%;height:1px;display:inline-block;border-top:1px solid black;"></div>
 		</div>
 	</div>
@@ -22,25 +22,25 @@
 
 		<div  class="tr  col-lg-12 col-md-12 col-sm-12 col-xs-12 inner-text" style="text-shadow:none;padding-top:0;">		
 
-				<h2 style="color:#00255B;  text-shadow: none;">Misión</h2>
-				<p>Innovar utilizando herramientas biotecnológicas para la mejora continua de la salud animal y colaborar así con la generación de alimentos para satisfacer la demanda de próximas generaciones.</p>
+				<h2 style="color:#00255B;  text-shadow: none;"><?php echo Textos::model()->GetText(385); ?></h2>
+				<p><?php echo Textos::model()->GetText(386); ?>.</p>
 
-				<h2 style="margin-top:12%;color:#00255B;text-shadow: none;">Visión</h2>
-				<p>Liderar la evolución de la salud animal integrada a las necesidades del ser humano.</p>
+				<h2 style="margin-top:12%;color:#00255B;text-shadow: none;"><?php echo Textos::model()->GetText(387); ?></h2>
+				<p><?php echo Textos::model()->GetText(388); ?></p>
 				
-				<h2 style="margin-top:12%;color:#00255B">Valores</h2>
+				<h2 style="margin-top:12%;color:#00255B"><?php echo Textos::model()->GetText(389); ?></h2>
 				
 				<!--PC-->
 				<div style="color:#8C8C8C">
 				<ul class="col-lg-12 col-md-12 col-sm-12 col-s-12" style="margin-top:3%;">
-					<li class="mision-li  col-lg-4 col-md-4 col-sm-12 col-xs-12" style="margin-bottom:2vh;"><img src="img/mision-tic.png"  />Innovación</li>
-					<li class="mision-li  col-lg-4 col-md-4 col-sm-12 col-xs-12" style="margin-bottom:2vh;"><img src="img/mision-tic.png"  />Cercanía</li>
-					<li class="mision-li col-lg-4 col-md-4 col-sm-12 col-xs-12" style="margin-bottom:2vh;"><img src="img/mision-tic.png"  />Flexibilidad</li>
-					<li class="mision-li  col-lg-4 col-md-4 col-sm-12 col-xs-12" style="margin-bottom:2vh;"><img src="img/mision-tic.png"  />Progreso</li>
-					<li class="mision-li  col-lg-4 col-md-4 col-sm-12 col-xs-12" style="margin-bottom:2vh;"><img src="img/mision-tic.png"  />Trabajo en equipo</li>
-					<li class="mision-li col-lg-4 col-md-4 col-sm-12 col-xs-12" style="margin-bottom:2vh;"><img src="img/mision-tic.png"  />Integridad</li>
-					<li class="mision-li  col-lg-4 col-md-4 col-sm-12 col-xs-12" style="margin-bottom:2vh;"><img src="img/mision-tic.png"  />Excelencia</li>
-					<li class="mision-li  col-lg-4 col-md-4 col-sm-12 col-xs-12" style="margin-bottom:2vh;"><img src="img/mision-tic.png"  />Responsabilidad</li>
+					<li class="mision-li  col-lg-4 col-md-4 col-sm-12 col-xs-12" style="margin-bottom:2vh;"><img src="img/mision-tic.png"  /><?php echo Textos::model()->GetText(390); ?></li>
+					<li class="mision-li  col-lg-4 col-md-4 col-sm-12 col-xs-12" style="margin-bottom:2vh;"><img src="img/mision-tic.png"  /><?php echo Textos::model()->GetText(391); ?></li>
+					<li class="mision-li col-lg-4 col-md-4 col-sm-12 col-xs-12" style="margin-bottom:2vh;"><img src="img/mision-tic.png"  /><?php echo Textos::model()->GetText(392); ?></li>
+					<li class="mision-li  col-lg-4 col-md-4 col-sm-12 col-xs-12" style="margin-bottom:2vh;"><img src="img/mision-tic.png"  /><?php echo Textos::model()->GetText(393); ?></li>
+					<li class="mision-li  col-lg-4 col-md-4 col-sm-12 col-xs-12" style="margin-bottom:2vh;"><img src="img/mision-tic.png"  /><?php echo Textos::model()->GetText(394); ?></li>
+					<li class="mision-li col-lg-4 col-md-4 col-sm-12 col-xs-12" style="margin-bottom:2vh;"><img src="img/mision-tic.png"  /><?php echo Textos::model()->GetText(395); ?></li>
+					<li class="mision-li  col-lg-4 col-md-4 col-sm-12 col-xs-12" style="margin-bottom:2vh;"><img src="img/mision-tic.png"  /><?php echo Textos::model()->GetText(396); ?></li>
+					<li class="mision-li  col-lg-4 col-md-4 col-sm-12 col-xs-12" style="margin-bottom:2vh;"><img src="img/mision-tic.png"  /><?php echo Textos::model()->GetText(397); ?></li>
 				</ul>
 				</div>
 
