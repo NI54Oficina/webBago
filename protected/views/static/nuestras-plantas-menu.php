286146d4b944bc1700fecd15b4f0e64e4a91ba81
NI54-oficina <frantrf@gmail.com>
2016-03-16 13:20:52 -0300

    13:20 pm 16-03 Coni
    Nuestras plantas menú interior

--- protected/views/static/nuestras-plantas-menu.php
+++ protected/views/static/nuestras-plantas-menu.php
@@ -1,48 +1,57 @@
-<section>
-			<div class="header-layout-aftosa  col-lg-12 col-md-12 col-sm-12 col-xs-12">
-				<h1 class="titulo-layout-quienes"><?php echo Textos::model()->GetText(14); ?></h1>
+<img id="logo-bago-mobile" class="hidden-lg hidden-md col-sm-12 col-xs-12" src="/webBago/img/logo-bago.png" style="opacity:0;">
+			<div class="header-layout-aftosa col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div style="width:100%;position:absolute;"><h1 class="titulo-layout-aftosa" style=""><?php echo Textos::model()->GetText(14); ?></h1></div>
+				<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/header-plantas.png" style="width:100vw;" />	
 			</div>
-	</section>
 	
-	<?php include('widget.php'); ?>
-	<div class=" col-lg-8 col-md-8 col-sm-12 col-xs-12  row " style="margin-left:6%;">
+	<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+	<div class="side-title">
+		<div style="display:inline-block;">
+		<h2  class="h2-aftosa">Institucional</h2>	
+		<div style="width:50%;height:1px;display:inline-block;border-top:1px solid black;"></div>
+		</div>
+	</div>
+	</div>
+
 	
-	<h2 class="h2-aftosa col-lg-8 col-md-8 col-sm-12 col-xs-12  row " style="text-decoration:none; font-weight:700; font-size:25px;">Tour Biogenesis 360 °</h2>
+<div class="div-texto-aftosa col-lg-5 col-md-5 col-sm-12 col-xs-12" style="text-align: left;">
+
+	<div class="tabla-aftosa col-lg-12 col-md-12 col-sm-12 col-xs-12">
 	
-		<div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 row" style="margin-bottom:3%;">
+	<div  class="tr  col-lg-12 col-md-12 col-sm-12 col-xs-12 inner-text" style="text-shadow:none;padding-top:0;margin-bottom:3%;">	
+	<h2 class="h2-aftosa" style="text-decoration:none; font-weight:700; font-size:25px;">Tour Biogenesis 360 °</h2>
+	
+		<div  class="tr  col-lg-12 col-md-12 col-sm-12 col-xs-12 inner-text" style="text-shadow:none;padding-top:0;">	
 			<a href="">
 				<img  class="col-lg-12 col-md-12 col-sm-12 col-xs-12" src="img/imagen-360-exterior.png"  />
 			</a>
 		</div>
 	
-		<div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 row " style="margin-bottom:3%;">
+		<div  class="tr  col-lg-12 col-md-12 col-sm-12 col-xs-12 inner-text" style="text-shadow:none;padding-top:0;margin-bottom:3%;">	
 			<a href="">
 				<img  class="col-lg-12 col-md-12 col-sm-12 col-xs-12" src="img/imagen-360-camara.png"  />
 			</a>
 		</div>
 
-		<div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 row " style="margin-bottom:3%;">
+		<div  class="tr  col-lg-12 col-md-12 col-sm-12 col-xs-12 inner-text" style="text-shadow:none;padding-top:0;margin-bottom:3%;">	
 			<a href="">
 				<img  class="col-lg-12 col-md-12 col-sm-12 col-xs-12" src="img/imagen-360-empaque.png"  />
 			</a>
 		</div>
 
-		<div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 row " style="margin-bottom:3%;">
+		<div  class="tr  col-lg-12 col-md-12 col-sm-12 col-xs-12 inner-text" style="text-shadow:none;padding-top:0;margin-bottom:3%;">	
 			<a href="">
 				<img  class="col-lg-12 col-md-12 col-sm-12 col-xs-12" src="img/imagen-360-sala.png"  />
 			</a>
 		</div>
 
-		<div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 row " style="margin-bottom:3%;">
+		<div  class="tr  col-lg-12 col-md-12 col-sm-12 col-xs-12 inner-text" style="text-shadow:none;padding-top:0;margin-bottom:3%;">	
 			<a href="">
 				<img  class="col-lg-12 col-md-12 col-sm-12 col-xs-12" src="img/imagen-360-pasillo.png"  />
 			</a>
 		</div>		
-	
-	
-	
-	
-	
-	
-	
+		</div>
+		</div>
 	</div>
+		
+	<?php include('widget.php'); ?>
