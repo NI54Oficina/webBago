363907c89508ba351a3adfc3c3b266a1f6fbbbd3
NI54-oficina <frantrf@gmail.com>
2016-03-16 14:29:57 -0300

    14:29 pm 16-03 Coni
    Nuestras plantas videos e imagénes

--- protected/views/static/nuestras-plantas-menu.php
+++ protected/views/static/nuestras-plantas-menu.php
@@ -3,7 +3,15 @@
 				<div style="width:100%;position:absolute;"><h1 class="titulo-layout-aftosa" style=""><?php echo Textos::model()->GetText(14); ?></h1></div>
 				<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/header-plantas.png" style="width:100vw;" />	
 			</div>
-
+	
+	<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+	<div class="side-title">
+		<div style="display:inline-block;">
+		<h2  class="h2-aftosa">Institucional</h2>	
+		<div style="width:50%;height:1px;display:inline-block;border-top:1px solid black;"></div>
+		</div>
+	</div>
+	</div>
 
 	
 <div class="div-texto-aftosa col-lg-5 col-md-5 col-sm-12 col-xs-12" style="text-align: left;">
@@ -11,34 +19,35 @@
 	<div class="tabla-aftosa col-lg-12 col-md-12 col-sm-12 col-xs-12">
 	
 	<div  class="tr  col-lg-12 col-md-12 col-sm-12 col-xs-12 inner-text" style="text-shadow:none;padding-top:0;margin-bottom:3%;">	
-	<h2 class="h2-aftosa" style="text-decoration:none; font-weight:700; font-size:25px;">Tour Biogenesis 360 °</h2>
+		
+		<h2 class="h2-aftosa" style="text-decoration:none; font-weight:700; font-size:25px;">Tour Biogenesis 360 °</h2>
 	
 		<div  class="tr  col-lg-12 col-md-12 col-sm-12 col-xs-12 inner-text" style="text-shadow:none;padding-top:0;margin-bottom:3%;">	
-			<a href="">
+			<a href="<?php echo Yii::app()->request->baseUrl; ?>/1080/Exterior/Exterior_1080.html" target="_blank">
 				<img  class="col-lg-12 col-md-12 col-sm-12 col-xs-12" src="img/imagen-360-exterior.png"  />
 			</a>
 		</div>
 	
 		<div  class="tr  col-lg-12 col-md-12 col-sm-12 col-xs-12 inner-text" style="text-shadow:none;padding-top:0;margin-bottom:3%;">	
-			<a href="">
+			<a href="<?php echo Yii::app()->request->baseUrl; ?>/1080/Camara Frig/Camara_1080.html" target="_blank">
 				<img  class="col-lg-12 col-md-12 col-sm-12 col-xs-12" src="img/imagen-360-camara.png"  />
 			</a>
 		</div>
 
 		<div  class="tr  col-lg-12 col-md-12 col-sm-12 col-xs-12 inner-text" style="text-shadow:none;padding-top:0;margin-bottom:3%;">	
-			<a href="">
+			<a href="<?php echo Yii::app()->request->baseUrl; ?>/1080/Empaque/Empaque_10800.html" target="_blank">
 				<img  class="col-lg-12 col-md-12 col-sm-12 col-xs-12" src="img/imagen-360-empaque.png"  />
 			</a>
 		</div>
 
 		<div  class="tr  col-lg-12 col-md-12 col-sm-12 col-xs-12 inner-text" style="text-shadow:none;padding-top:0;margin-bottom:3%;">	
-			<a href="">
+			<a href="<?php echo Yii::app()->request->baseUrl; ?>/1080/Sala Big Tubes/Sala_Big_Tubes_1080.html" target="_blank">
 				<img  class="col-lg-12 col-md-12 col-sm-12 col-xs-12" src="img/imagen-360-sala.png"  />
 			</a>
 		</div>
 
 		<div  class="tr  col-lg-12 col-md-12 col-sm-12 col-xs-12 inner-text" style="text-shadow:none;padding-top:0;margin-bottom:3%;">	
-			<a href="">
+			<a href="<?php echo Yii::app()->request->baseUrl; ?>/1080/Pasillo/Pasillo_1080.html" target="_blank">
 				<img  class="col-lg-12 col-md-12 col-sm-12 col-xs-12" src="img/imagen-360-pasillo.png"  />
 			</a>
 		</div>		
