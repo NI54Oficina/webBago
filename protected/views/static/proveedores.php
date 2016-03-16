f1e10daaeb7eda941b0626c2510cc738c8eed3fa
melmiranda <melania.smc@hotmail.com>
2016-03-16 12:47:19 -0300

    12:46 pm 16-03-2016 Melania
    proveedores

--- protected/views/static/proveedores.php
+++ protected/views/static/proveedores.php
@@ -1,6 +1,6 @@
 <img id="logo-bago-mobile" class="hidden-lg hidden-md col-sm-12 col-xs-12" src="/webBago/img/logo-bago.png" style="opacity:0;">
 			<div class="header-layout-aftosa col-lg-12 col-md-12 col-sm-12 col-xs-12">
-				<div style="width:100%;position:absolute;"><h1 class="titulo-layout-aftosa" style="">Info para proveedores</h1></div>
+				<div style="width:100%;position:absolute;"><h1 class="titulo-layout-aftosa" style=""><?php echo Textos::model()->GetText(18); ?></h1></div>
 				<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/header-proveedores.png" style="width:100vw;" />	
 			</div>
 	
@@ -8,7 +8,7 @@
 	<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
 	<div class="side-title">
 		<div style="display:inline-block;">
-		<h2  class="h2-aftosa">Institucional</h2>	
+		<h2  class="h2-aftosa"><?php echo Textos::model()->GetText(347); ?></h2>	
 		<div style="width:50%;height:1px;display:inline-block;border-top:1px solid black;"></div>
 		</div>
 	</div>
@@ -22,10 +22,10 @@
 
 			<!--PC-->
 				<ul class="col-lg-12 col-md-12 col-sm-12 col-xs-12 " style="margin-top:3%;">
-					<li class="col-lg-12 col-md-12 col-sm-12 col-xs-12 proveedores-li"><a style="font-size: 16px;" href=""><img src="img/icono-descarga.png"  />Constancia de Inscripción AFIP (CUIT)</a></li>
-					<li class="col-lg-12 col-md-12 col-sm-12 col-xs-12 proveedores-li"><a style="font-size: 16px;" href=""><img src="img/icono-descarga.png"  />Impuesto sobre los Ingresos Brutos Convenio Multilateral (cm01-cm02)</a></li>
-					<li class="col-lg-12 col-md-12 col-sm-12 col-xs-12 proveedores-li"><a style="font-size: 16px;" href=""><img src="img/icono-descarga.png"  />Constancia de Inscripción IIBB</a></li>
-					<li class="col-lg-12 col-md-12 col-sm-12 col-xs-12 proveedores-li"><a style="font-size: 16px;" href=""><img src="img/icono-descarga.png"  />Convenio Multilateral (form cm05)</a></li>
+					<li class="col-lg-12 col-md-12 col-sm-12 col-xs-12 proveedores-li"><a style="font-size: 16px;" href=""><img src="img/icono-descarga.png"  /><?php echo Textos::model()->GetText(380); ?></a></li>
+					<li class="col-lg-12 col-md-12 col-sm-12 col-xs-12 proveedores-li"><a style="font-size: 16px;" href=""><img src="img/icono-descarga.png"  /><?php echo Textos::model()->GetText(381); ?></a></li>
+					<li class="col-lg-12 col-md-12 col-sm-12 col-xs-12 proveedores-li"><a style="font-size: 16px;" href=""><img src="img/icono-descarga.png"  /><?php echo Textos::model()->GetText(382); ?></a></li>
+					<li class="col-lg-12 col-md-12 col-sm-12 col-xs-12 proveedores-li"><a style="font-size: 16px;" href=""><img src="img/icono-descarga.png"  /><?php echo Textos::model()->GetText(383); ?></a></li>
 				</ul>		
 	</div>
 	</div>
