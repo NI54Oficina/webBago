c98ac49d44e4e6e3683e4c50612f745cfdd0690b
NI54-oficina <frantrf@gmail.com>
2016-03-16 11:33:32 -0300

    11.33 am 16-03 Coni
    Layout Misión, visión y valores

--- protected/views/static/mision-vision.php
+++ protected/views/static/mision-vision.php
@@ -1,28 +1,27 @@
-	
-	<section>
-			<div class="header-layout-mision  col-lg-12 col-md-12 col-sm-12 col-xs-12">
-				<h1 class="titulo-layout-quienes">Misión, visión y valores</h1>
+<img id="logo-bago-mobile" class="hidden-lg hidden-md col-sm-12 col-xs-12" src="/webBago/img/logo-bago.png" style="opacity:0;">
+			<div class="header-layout-aftosa col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div style="width:100%;position:absolute;"><h1 class="titulo-layout-aftosa" style="">Misión, visión y valores</h1></div>
+				<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/valores.png" style="width:100vw;" />	
 			</div>
 	</section>
 
-	<?php include('widget.php'); ?>
-<div class="div-texto-aftosa col-lg-9 col-md-9 col-sm-12 col-xs-12">
-	
-	<div>
-	<div class="tabla-aftosa col-lg-2 col-md-2 col-sm-2 col-xs-2 hidden-xs hidden-sm" >
-		<div  class="tr">
-			<div>
-				<h2 class="h2-aftosa" style="text-shadow: none;"><?php echo Textos::model()->GetText(347); ?> <br /><br /><br /><br /><br /></h2>
-			</div>
 
+	<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+	<div class="side-title">
+		<div style="display:inline-block;">
+		<h2  class="h2-aftosa">Institucional</h2>	
+		<div style="width:50%;height:1px;display:inline-block;border-top:1px solid black;"></div>
 		</div>
-		
 	</div>
-		<div class="col-lg-1 col-md-1 col-sm-1 col-xs-1 "></div>
+	</div>
+	
+	
+<div class="div-texto-aftosa col-lg-5 col-md-5 col-sm-12 col-xs-12" style="text-align: left;">
+		
 	<div class="tabla-aftosa col-lg-10 col-md-10 col-sm-12 col-xs-12" style="text-align: left;">
-		<div  class="tr">
-			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="    text-shadow: none;">
-			<h2 class="h2-aftosa hidden-md hidden-lg" style=" text-shadow: none;">Institucional <br /><br /><br /></h2>
+
+		<div  class="tr  col-lg-12 col-md-12 col-sm-12 col-xs-12 inner-text" style="text-shadow:none;padding-top:0;">		
+
 				<h2 style="color:#00255B;  text-shadow: none;">Misión</h2>
 				<p>Innovar utilizando herramientas biotecnológicas para la mejora continua de la salud animal y colaborar así con la generación de alimentos para satisfacer la demanda de próximas generaciones.</p>
 
@@ -33,39 +32,25 @@
 				
 				<!--PC-->
 				<div style="color:#8C8C8C">
-				<ul class="col-lg-12 col-md-12 col-sm-12 col-xs-12 hidden-xs hidden-sm" style="margin-top:3%;">
-					<li class="mision-li  col-lg-4 col-md-4 col-sm-6 col-xs-6"><img src="img/mision-tic.png"  />Innovación</li>
-					<li class="mision-li  col-lg-4 col-md-4 col-sm-6 col-xs-6"><img src="img/mision-tic.png"  />Cercanía</li>
-					<li class="mision-li col-lg-4 col-md-4 col-sm-6 col-xs-6"><img src="img/mision-tic.png"  />Flexibilidad</li>
-				</ul>
-				
-				<ul class="mision-ul col-lg-12 col-md-12 col-sm-12 col-xs-12 hidden-xs hidden-sm">
-					<li class="mision-li  col-lg-4 col-md-4 col-sm-6 col-xs-6 hidden-xs hidden-md"><img src="img/mision-tic.png"  />Progreso</li>
-					<li class="mision-li  col-lg-4 col-md-4 col-sm-6 col-xs-6"><img src="img/mision-tic.png"  />Trabajo en equipo</li>
-					<li class="mision-li col-lg-4 col-md-4 col-sm-6 col-xs-6"><img src="img/mision-tic.png"  />Integridad</li>
-				</ul>
-				
-				<ul class="mision-ul col-lg-12 col-md-12 col-sm-12 col-xs-12 hidden-xs hidden-sm">
-					<li class="mision-li  col-lg-4 col-md-4 col-sm-6 col-xs-6"><img src="img/mision-tic.png"  />Excelencia</li>
-					<li class="mision-li  col-lg-5 col-md-4 col-sm-6 col-xs-6"><img src="img/mision-tic.png"  />Responsabilidad</li>
+				<ul class="col-lg-12 col-md-12 col-sm-12 col-s-12" style="margin-top:3%;">
+					<li class="mision-li  col-lg-4 col-md-4 col-sm-12 col-xs-12" style="margin-bottom:2vh;"><img src="img/mision-tic.png"  />Innovación</li>
+					<li class="mision-li  col-lg-4 col-md-4 col-sm-12 col-xs-12" style="margin-bottom:2vh;"><img src="img/mision-tic.png"  />Cercanía</li>
+					<li class="mision-li col-lg-4 col-md-4 col-sm-12 col-xs-12" style="margin-bottom:2vh;"><img src="img/mision-tic.png"  />Flexibilidad</li>
+					<li class="mision-li  col-lg-4 col-md-4 col-sm-12 col-xs-12" style="margin-bottom:2vh;"><img src="img/mision-tic.png"  />Progreso</li>
+					<li class="mision-li  col-lg-4 col-md-4 col-sm-12 col-xs-12" style="margin-bottom:2vh;"><img src="img/mision-tic.png"  />Trabajo en equipo</li>
+					<li class="mision-li col-lg-4 col-md-4 col-sm-12 col-xs-12" style="margin-bottom:2vh;"><img src="img/mision-tic.png"  />Integridad</li>
+					<li class="mision-li  col-lg-4 col-md-4 col-sm-12 col-xs-12" style="margin-bottom:2vh;"><img src="img/mision-tic.png"  />Excelencia</li>
+					<li class="mision-li  col-lg-4 col-md-4 col-sm-12 col-xs-12" style="margin-bottom:2vh;"><img src="img/mision-tic.png"  />Responsabilidad</li>
 				</ul>
 				</div>
-				<!--MOBILE-->
-				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 hidden-md hidden-lg mob-dv" style="margin-top:3%;">
-					<p style="margin-top:2%;font-size:22px;" class="mision-li  col-lg-4 col-md-4 col-sm-12 col-xs-12"><img  style="width:3%;" src="img/mision-tic.png"  />Innovación</p>
-					<p style="margin-top:2%;font-size:22px;" class="mision-li  col-lg-4 col-md-4 col-sm-12 col-xs-12"><img  style="width:3%;" src="img/mision-tic.png"  />Cercanía</p>
-					<p style="margin-top:2%;font-size:22px;" class="mision-li col-lg-4 col-md-4  col-sm-12 col-xs-12"><img  style="width:3%;" src="img/mision-tic.png"  />Flexibilidad</p>
-					<p style="margin-top:2%;font-size:22px;" class="mision-li  col-lg-4 col-md-4 col-sm-12 col-xs-12 "><img style="width:3%;"  src="img/mision-tic.png"  />Progreso</p>
-					<p style="margin-top:2%;font-size:22px;" class="mision-li  col-lg-4 col-md-4 col-sm-12 col-xs-12"><img  style="width:3%;" src="img/mision-tic.png"  />Trabajo en equipo</p>
-					<p style="margin-top:2%;font-size:22px;" class="mision-li col-lg-4 col-md-4  col-sm-12 col-xs-12"><img  style="width:3%;" src="img/mision-tic.png"  />Integridad</p>
-					<p style="margin-top:2%;font-size:22px;" class="mision-li  col-lg-4 col-md-4 col-sm-12 col-xs-12"><img  style="width:3%;" src="img/mision-tic.png"  />Excelencia</p>
-					<p style="margin-top:2%;font-size:22px;" class="mision-li  col-lg-4 col-md-4 col-sm-12 col-xs-12"><img  style="width:3%;" src="img/mision-tic.png"  />Responsabilidad</p>
-				</div>
+
 				
 			</div>
-		</div>
+
 	</div>
-	</div>
+
 	
 
 </div>
+
+<?php include('widget.php'); ?>
