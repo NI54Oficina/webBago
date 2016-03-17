7be00225c5b5e0d200692567f2d706ee9c9fb0e9
NI54-oficina <frantrf@gmail.com>
2016-03-17 18:45:18 -0300

    18:57 pm 17-03 Coni

--- protected/views/static/mision-vision.php
+++ protected/views/static/mision-vision.php
@@ -40,7 +40,7 @@
 					<li class="mision-li  col-lg-4 col-md-12 col-sm-12 col-xs-12" style="margin-bottom:2vh;"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/mision-tic.png"  /><?php echo Textos::model()->GetText(394); ?></li>
 					<li class="mision-li col-lg-4 col-md-12 col-sm-12 col-xs-12" style="margin-bottom:2vh;"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/mision-tic.png"  /><?php echo Textos::model()->GetText(395); ?></li>
 					<li class="mision-li  col-lg-4 col-md-12 col-sm-12 col-xs-12" style="margin-bottom:2vh;"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/mision-tic.png"  /><?php echo Textos::model()->GetText(396); ?></li>
-					<li class="mision-li  col-lg-4 col-md-12 col-sm-12 col-xs-12" style="margin-bottom:2vh;"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/mision-tic.png"  /><?php echo Textos::model()->GetText(397); ?></li>
+					<li class="mision-li  col-lg-4-col-md-12 col-sm-12 col-xs-12" style="margin-bottom:2vh;"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/mision-tic.png"  /><?php echo Textos::model()->GetText(397); ?></li>
 				</ul>
 				</div>
 
