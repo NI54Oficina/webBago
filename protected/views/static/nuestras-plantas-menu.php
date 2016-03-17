19fd34d598c72b5c71ff6b07a566f100c6c6bbdf
NI54-oficina <frantrf@gmail.com>
2016-03-17 17:38:56 -0300

    17.40 pm 17-03 Coni
    Url img nuestras plantas menu

--- protected/views/static/nuestras-plantas-menu.php
+++ protected/views/static/nuestras-plantas-menu.php
@@ -17,31 +17,31 @@
 	
 		<div  class="  col-lg-6 col-md-6 col-sm-12 col-xs-12 inner-text container-lista-360" style="text-shadow:none;padding-top:0;margin-bottom:2.5%;">	
 			<a href="<?php echo Yii::app()->request->baseUrl; ?>/1080/Exterior/Exterior_1080.html" target="_blank">
-				<img  src="img/imagen-360-exterior.png"  />
+				<img  src="<?php echo Yii::app()->request->baseUrl; ?>/img/imagen-360-exterior.png"  />
 			</a>
 		</div>
 	
 		<div  class="col-lg-6 col-md-6 col-sm-12 col-xs-12 inner-text container-lista-360" style="text-shadow:none;padding-top:0;margin-bottom:2.5%;">	
 			<a href="<?php echo Yii::app()->request->baseUrl; ?>/1080/Camara Frig/Camara_1080.html" target="_blank">
-				<img   src="img/imagen-360-camara.png"  />
+				<img   src="<?php echo Yii::app()->request->baseUrl; ?>/img/imagen-360-camara.png"  />
 			</a>
 		</div>
 
 		<div  class="col-lg-6 col-md-6 col-sm-12 col-xs-12 inner-text container-lista-360" style="text-shadow:none;padding-top:0;margin-bottom:2.5%;">	
 			<a href="<?php echo Yii::app()->request->baseUrl; ?>/1080/Empaque/Empaque_10800.html" target="_blank">
-				<img  src="img/imagen-360-empaque.png"  />
+				<img  src="<?php echo Yii::app()->request->baseUrl; ?>/img/imagen-360-empaque.png"  />
 			</a>
 		</div>
 
 		<div  class="col-lg-6 col-md-6 col-sm-12 col-xs-12 inner-text container-lista-360" style="text-shadow:none;padding-top:0;margin-bottom:2.5%;">	
 			<a href="<?php echo Yii::app()->request->baseUrl; ?>/1080/Sala Big Tubes/Sala_Big_Tubes_1080.html" target="_blank">
-				<img  src="img/imagen-360-sala.png"  />
+				<img  src="<?php echo Yii::app()->request->baseUrl; ?>/img/imagen-360-sala.png"  />
 			</a>
 		</div>
 
 		<div  class="col-lg-6 col-md-6 col-sm-12 col-xs-12 inner-text container-lista-360" style="text-shadow:none;padding-top:0;margin-bottom:2.5%;">	
 			<a href="<?php echo Yii::app()->request->baseUrl; ?>/1080/Pasillo/Pasillo_1080.html" target="_blank">
-				<img  src="img/imagen-360-pasillo.png"  />
+				<img  src="<?php echo Yii::app()->request->baseUrl; ?>/img/imagen-360-pasillo.png"  />
 			</a>
 		</div>		
 		</div>
