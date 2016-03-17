374422cf03812cd2e60860cd800d687195c81f5d
NI54-oficina <frantrf@gmail.com>
2016-03-17 16:59:06 -0300

    17:00 pm 17-03 Coni
    Url

--- protected/views/static/widget.php
+++ protected/views/static/widget.php
@@ -1,13 +1,20 @@
 
 <div class="col-lg-4 col-md-4 col-sm-3 col-xs-3 visible-desktop hidden-sm hidden-xs" id="table" style="text-align:left;">
-	<div class="tr-widget col-lg-12 col-md-12 col-sm-12 col-xs-12"><a href=""><div class="td-widget col-lg-2 col-md-2 col-sm-2 col-xs-2" ><div class="img-widget icono-quienes"></div></div><div><a href=""><p class="p-widget"><?php echo Textos::model()->GetText(6); ?></p></div></a></div>
-	<div class="tr-widget col-lg-12 col-md-12 col-sm-12 col-xs-12"><a href=""><div class="td-widget  col-lg-2 col-md-2 col-sm-2 col-xs-2" ><div class="img-widget icono-mision"></div></div><div><p class="p-widget"><?php echo Textos::model()->GetText(8); ?></p></div></a></div>
-	<div class="tr-widget col-lg-12 col-md-12 col-sm-12 col-xs-12"><a href=""><div class="td-widget  col-lg-2 col-md-2 col-sm-2 col-xs-2" ><div class="img-widget icono-trayectoria"></div></div><div><p class="p-widget">	<?php echo Textos::model()->GetText(12); ?></p></div></a></div>
-	<div class="tr-widget col-lg-12 col-md-12 col-sm-12 col-xs-12"><a href=""><div class="td-widget col-lg-2 col-md-2 col-sm-2 col-xs-2" ><div class="img-widget icono-aftosa"></div></div><div> <p class="p-widget"><?php echo Textos::model()->GetText(10); ?></p></div></a></div>
-	<div class="tr-widget col-lg-12 col-md-12 col-sm-12 col-xs-12"> <a href=""><div class="td-widget  col-lg-2 col-md-2 col-sm-2 col-xs-2" ><div class="img-widget icono-plantas"></div></div><div><p class="p-widget"><?php echo Textos::model()->GetText(14); ?></p></div></a></div>
-	<div class="tr-widget col-lg-12 col-md-12 col-sm-12 col-xs-12"><a href=""><div class="td-widget  col-lg-2 col-md-2 col-sm-2 col-xs-2" ><div class="img-widget icono-rse"></div></div><div> <p class="p-widget"><?php echo Textos::model()->GetText(16); ?></p></div></a></div>
-	<div class="tr-widget col-lg-12 col-md-12 col-sm-12 col-xs-12"><a href=""><div class="td-widget  col-lg-2 col-md-2 col-sm-2 col-xs-2" ><div class="img-widget icono-proveedores"></div></div><div><p class="p-widget"><?php echo Textos::model()->GetText(430); ?></p></div></a></div>
-	<div class="tr-widget col-lg-12 col-md-12 col-sm-12 col-xs-12"><a href=""><div class="td-widget  col-lg-2 col-md-2 col-sm-2 col-xs-2" ><div class="img-widget icono-prensa"></div></div><div><p class="p-widget"><?php echo Textos::model()->GetText(20); ?></p></div></a></div>
+	<div class="tr-widget col-lg-12 col-md-12 col-sm-12 col-xs-12"><a href="<?php echo Yii::app()->paisChecker->getBaseUrl(true); ?>/quienes-somos"><div class="td-widget col-lg-2 col-md-2 col-sm-2 col-xs-2" ><div class="img-widget icono-quienes"></div></div><div><a href=""><p class="p-widget"><?php echo Textos::model()->GetText(6); ?></p></div></a></div>
+	
+	<div class="tr-widget col-lg-12 col-md-12 col-sm-12 col-xs-12"><a href="<?php echo Yii::app()->paisChecker->getBaseUrl(true); ?>/mision-vision"><div class="td-widget  col-lg-2 col-md-2 col-sm-2 col-xs-2" ><div class="img-widget icono-mision"></div></div><div><p class="p-widget"><?php echo Textos::model()->GetText(8); ?></p></div></a></div>
+	
+	<div class="tr-widget col-lg-12 col-md-12 col-sm-12 col-xs-12"><a href="<?php echo Yii::app()->paisChecker->getBaseUrl(true); ?>/lucha-contra-aftosa"><div class="td-widget  col-lg-2 col-md-2 col-sm-2 col-xs-2" ><div class="img-widget icono-trayectoria"></div></div><div><p class="p-widget">	<?php echo Textos::model()->GetText(12); ?></p></div></a></div>
+	
+	<div class="tr-widget col-lg-12 col-md-12 col-sm-12 col-xs-12"><a href="<?php echo Yii::app()->paisChecker->getBaseUrl(true); ?>/trayectoria-hitos"><div class="td-widget col-lg-2 col-md-2 col-sm-2 col-xs-2" ><div class="img-widget icono-aftosa"></div></div><div> <p class="p-widget"><?php echo Textos::model()->GetText(10); ?></p></div></a></div>
+	
+	<div class="tr-widget col-lg-12 col-md-12 col-sm-12 col-xs-12"> <a href="<?php echo Yii::app()->paisChecker->getBaseUrl(true); ?>/nuestras-plantas"><div class="td-widget  col-lg-2 col-md-2 col-sm-2 col-xs-2" ><div class="img-widget icono-plantas"></div></div><div><p class="p-widget"><?php echo Textos::model()->GetText(14); ?></p></div></a></div>
+	
+	<div class="tr-widget col-lg-12 col-md-12 col-sm-12 col-xs-12"><a href="<?php echo Yii::app()->paisChecker->getBaseUrl(true); ?>/rse"><div class="td-widget  col-lg-2 col-md-2 col-sm-2 col-xs-2" ><div class="img-widget icono-rse"></div></div><div> <p class="p-widget"><?php echo Textos::model()->GetText(16); ?></p></div></a></div>
+	
+	<div class="tr-widget col-lg-12 col-md-12 col-sm-12 col-xs-12"><a href="<?php echo Yii::app()->paisChecker->getBaseUrl(true); ?>/proveedores"><div class="td-widget  col-lg-2 col-md-2 col-sm-2 col-xs-2" ><div class="img-widget icono-proveedores"></div></div><div><p class="p-widget"><?php echo Textos::model()->GetText(430); ?></p></div></a></div>
+	
+	<div class="tr-widget col-lg-12 col-md-12 col-sm-12 col-xs-12"><a href="<?php echo Yii::app()->paisChecker->getBaseUrl(true); ?>/prensa"><div class="td-widget  col-lg-2 col-md-2 col-sm-2 col-xs-2" ><div class="img-widget icono-prensa"></div></div><div><p class="p-widget"><?php echo Textos::model()->GetText(20); ?></p></div></a></div>
 
 
 </div>
