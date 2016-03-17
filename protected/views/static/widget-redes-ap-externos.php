e659c18ffc32e7e062815d2fd7c4933d0cd51c06
melmiranda <melania.smc@hotmail.com>
2016-03-17 12:25:54 -0300

    12:25 am 17-03-2016 Mel
    -Widgets 11/12

--- protected/views/static/widget-redes-ap-externos.php
+++ protected/views/static/widget-redes-ap-externos.php
@@ -1,4 +1,4 @@
-<p style="text-align:center;">Compartir en</p>
+<p style="text-align:center;"><?php echo Textos::model()->GetText(452); ?></p>
 		<p  style="text-align:center;border-bottom:1px solid #0D553F;width:15%;margin:auto;margin-top:-2%;"></p>
 		
 		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="margin: auto; text-align: center; margin-top: 7%;margin-left:25%;">
@@ -29,43 +29,43 @@
 			</a>
 		</div>
 		
-		<p style="text-align:center;margin-top:85%;">Registrado en</p>
+		<p style="text-align:center;margin-top:85%;"><?php echo Textos::model()->GetText(453); ?></p>
 		<p  style="text-align:center;border-bottom:1px solid #0D553F;width:15%;margin:auto;margin-top:-2%;"></p>
 		
 		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="margin: auto; text-align: center; margin-top: 8%;"> 
 
 			<div>
-				<img src="img/banderitas-01.png" style="display:inline-block; width: 11%; margin-right: 2%; vertical-align: middle;" /> <p style="display:inline-block;vertical-align: middle; margin-top: 3%;">Argentina</p>
+				<img src="img/banderitas-01.png" style="display:inline-block; width: 11%; margin-right: 2%; vertical-align: middle;" /> <p style="display:inline-block;vertical-align: middle; margin-top: 3%;"><?php echo Textos::model()->GetText(410); ?></p>
 			</div>
 			
 			<p class="col-lg-7 col-md-7 col-sm-12 col-xs-12" style="border-bottom:1px solid #CCCCCC; margin-left: 31%; margin-top: 4%;"></p>
 			
 			<div style="margin-left: -8%;">
-				<img src="img/banderitas-03.png" style="width: 11%; margin-right: 2%; vertical-align: middle;" /> <p style="display:inline-block;vertical-align: middle; margin-top: 3%;">Brasil</p>		
+				<img src="img/banderitas-03.png" style="width: 11%; margin-right: 2%; vertical-align: middle;" /> <p style="display:inline-block;vertical-align: middle; margin-top: 3%;"><?php echo Textos::model()->GetText(412); ?></p>		
 			</div>
 			
 			<p class="col-lg-7 col-md-7 col-sm-12 col-xs-12" style="border-bottom:1px solid #CCCCCC; margin-left: 31%; margin-top: 4%;"></p>
 			
 			<div style="margin-left: -6%;">
-				<img src="img/banderitas-02.png" style="width: 11%; margin-right: 2%; vertical-align: middle;" /> <p style="display:inline-block;vertical-align: middle; margin-top: 3%;">Bolivia</p>		
+				<img src="img/banderitas-02.png" style="width: 11%; margin-right: 2%; vertical-align: middle;" /> <p style="display:inline-block;vertical-align: middle; margin-top: 3%;"><?php echo Textos::model()->GetText(411); ?></p>		
 			</div>
 			
 			<p class="col-lg-7 col-md-7 col-sm-12 col-xs-12" style="border-bottom:1px solid #CCCCCC; margin-left: 31%; margin-top: 4%;"></p>
 			
 			<div style="margin-left: 2%;">
-				<img src="img/banderitas-08.png" style="width: 11%; margin-right: 2%; vertical-align: middle;" /> <p style="display:inline-block;vertical-align: middle; margin-top: 3%;">Honduras</p>		
+				<img src="img/banderitas-08.png" style="width: 11%; margin-right: 2%; vertical-align: middle;" /> <p style="display:inline-block;vertical-align: middle; margin-top: 3%;"><?php echo Textos::model()->GetText(417); ?></p>		
 			</div>
 			
 			<p class="col-lg-7 col-md-7 col-sm-12 col-xs-12" style="border-bottom:1px solid #CCCCCC; margin-left: 31%; margin-top: 4%;"></p>
 			
 			<div style="margin-left: 2%;">
-				<img src="img/banderitas-12.png" style="width: 11%; margin-right: 2%; vertical-align: middle;" /> <p style="display:inline-block;vertical-align: middle; margin-top: 3%;">Panamá</p>		
+				<img src="img/banderitas-12.png" style="width: 11%; margin-right: 2%; vertical-align: middle;" /> <p style="display:inline-block;vertical-align: middle; margin-top: 3%;"><?php echo Textos::model()->GetText(420); ?></p>		
 			</div>
 			
 			<p class="col-lg-7 col-md-7 col-sm-12 col-xs-12" style="border-bottom:1px solid #CCCCCC; margin-left: 31%; margin-top: 4%;"></p>
 			
 			<div style="margin-left: 2%;">
-				<img src="img/banderitas-13.png" style="width: 11%; margin-right: 2%; vertical-align: middle;" /> <p style="display:inline-block;vertical-align: middle; margin-top: 3%;">Paraguay</p>		
+				<img src="img/banderitas-13.png" style="width: 11%; margin-right: 2%; vertical-align: middle;" /> <p style="display:inline-block;vertical-align: middle; margin-top: 3%;"><?php echo Textos::model()->GetText(421); ?></p>		
 			</div>
 			</div>
 	</div>
