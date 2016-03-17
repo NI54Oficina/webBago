e3169af3e8c4f98dcaa9658036ee1916a54b0815
Fran <fran@ni54.com>
2016-03-17 17:08:21 -0300

    17:08
    con suerte arreglado lo de las urls de los paises

--- protected/controllers/PaisController.php
+++ protected/controllers/PaisController.php
@@ -177,9 +177,14 @@ class PaisController extends Controller
 		$_SESSION["lng"]= $lng;
 		$_SESSION["pais"]= $id;
 		$_SESSION["short"]= Pais::model()->findByPk($id)->short;
-		
-		header("Location: ".$_SERVER['HTTP_REFERER']);
-		die();
+		if(isset($_SESSION["redirectURL"])){
+			header("Location: http://".$_SERVER['SERVER_NAME']."/".$_SESSION["webRoot"]."/".$_SESSION["redirectURL"]);
+			$_SESSION["redirectURL"]=null;
+			exit();
+		}else{
+			header("Location: ".$_SERVER['HTTP_REFERER']);
+			die();
+		}
 	}
 
 	/**
