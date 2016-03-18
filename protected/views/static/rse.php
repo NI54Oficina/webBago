42356f04bd72eaab4c2614f57f9990a7c87c2190
NI54-oficina <frantrf@gmail.com>
2016-03-18 18:05:08 -0300

    18:05 pm 18-03 Coni
    Rse y prensa noticias

--- protected/views/static/rse.php
+++ protected/views/static/rse.php
@@ -70,82 +70,91 @@
 				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 animate">
 					<div class="btn">
 						<div class="toggleIcon"></div>
-							<div class="ttl">2012</div>
+							<div class="ttl"><?php $notas =Notas::model()->findAllByAttributes(array('seccion'=>"rse"),"YEAR(fecha)=2016");?> 2016</div>
 					</div>
+					<?php echo count($notas); 
+					foreach($notas as $nota){
+					?>
 						<ul>
-							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p>22 de Noviembre de 2012  <div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse">Encuentro Biosolidario Tandil x Tandil </h1></a></li>
+							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p><?php echo $nota->fecha; ?> <div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse"><?php echo $nota->titulo; ?> </h1> <p><?php echo $nota->bajada; ?></p></a></li>
 						</ul>
+					<?php } ?>
 					</div>
 					
 				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 animate">
 					<div class="btn">
 						<div class="toggleIcon"></div>
-							<div class="ttl">2010</div>
+							<div class="ttl"><?php $notas =Notas::model()->findAllByAttributes(array('seccion'=>"rse"),"YEAR(fecha)=2012");?>  2012</div>
 					</div>
+					<?php echo count($notas); 
+					foreach($notas as $nota){
+					?>
 						<ul>
-							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p>13 de Noviembre de 2010 <div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse">Encuentro Biosolidario del Bicentenario </h1></a></li>
+							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p><?php echo $nota->fecha; ?>  <div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse"><?php echo $nota->titulo; ?> </h1> <p><?php echo $nota->bajada; ?></p></a></li>
 						</ul>
-					</div>
-					
-					
-				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 animate">
-					<div class="btn">
-						<div class="toggleIcon"></div>
-							<div class="ttl">2009</div>
-					</div>
-						<ul>
-							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p>09 de Octubre de 2009<div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse">Encuentro Biosolidario: Paraguay x Paraguay y su gente  </h1></a></li>
-							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p>04 de Julio de 2009<div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse">Encuentro Biosolidario Salta x Salta </h1></a></li>
-						</ul>
-					</div>
-					
-					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 animate">
-					<div class="btn">
-						<div class="toggleIcon"></div>
-							<div class="ttl">2008</div>
-					</div>
-						<ul>
-							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p>14 de Noviembre de 2008<div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse">5º Encuentro Biosolidario de Uruguay </h1></a></li>
-							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p>01 de Noviembre de 2008<div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse">Encuentro Biosolidario La Paz y Feliciano</h1></a></li>
-						</ul>
-					</div>
-		
-
-				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 animate">
-					<div class="btn">
-						<div class="toggleIcon"></div>
-							<div class="ttl">2007</div>
-					</div>
-						<ul>
-							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p>09 de Noviembre de 2007<div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse">4º Encuentro Biosolidario de Uruguay  </h1></a></li>
-							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p>26 de Abril de 2007<div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse">5º Encuentro Biosolidario a beneficio de Misiones Rurales Argentinas  </h1></a></li>
-						</ul>
+					<?php } ?>
 					</div>
 					
 
 				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 animate">
 					<div class="btn">
 						<div class="toggleIcon"></div>
-							<div class="ttl">2006</div>
+							<div class="ttl"><?php $notas =Notas::model()->findAllByAttributes(array('seccion'=>"rse"),"YEAR(fecha)=2010");?>2010</div>
 					</div>
+					<?php echo count($notas); 
+					foreach($notas as $nota){
+					?>
 						<ul>
-							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p>10 de Noviembre de 2006<div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse">3º Encuentro Biosolidario de Uruguay </h1></a></li>
-							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p>10 de Junio de 2006<div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse">Encuentro Biosolidario Rafaela x Rafaela y su región </h1></a></li>
+							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p><?php echo $nota->fecha; ?><div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse"><?php echo $nota->titulo; ?> </h1><p><?php echo $nota->bajada; ?></p></a></li>
 						</ul>
+					<?php } ?>
 					</div>
+					
 					
 
 				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 animate">
 					<div class="btn">
 						<div class="toggleIcon"></div>
-							<div class="ttl">2005</div>
+							<div class="ttl"><?php $notas =Notas::model()->findAllByAttributes(array('seccion'=>"rse"),"YEAR(fecha)=2009");?>2009</div>
 					</div>
+					<?php echo count($notas); 
+					foreach($notas as $nota){
+					?>
 						<ul>
-							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p>03 de Noviembre de 2005<div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse">2º Encuentro Biosolidario de Uruguay </h1></a></li>
-							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p>07 de Octubre de 2005<div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse">4º Encuentro Biosolidario de Uruguay  </h1></a></li>
-							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p>11 de Junio de 2005<div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse">Encuentro Biosolidario NEA x Solidagro </h1></a></li>
-							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p>23 de Abril de 2005<div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse">Encuentro Biosolidario Tapalqué x Tapalqué </h1></a></li>
+							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p><?php echo $nota->fecha; ?><div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse"><?php echo $nota->titulo; ?> </h1><p><?php echo $nota->bajada; ?></p></a></li>
 						</ul>
+					<?php } ?>
+					</div>
+					
+						
+	
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 animate">
+					<div class="btn">
+						<div class="toggleIcon"></div>
+							<div class="ttl"><?php $notas =Notas::model()->findAllByAttributes(array('seccion'=>"rse"),"YEAR(fecha)=2008");?>2008</div>
+					</div>
+					<?php echo count($notas); 
+					foreach($notas as $nota){
+					?>
+						<ul>
+							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p><?php echo $nota->fecha; ?><div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse"><?php echo $nota->titulo; ?> </h1><p><?php echo $nota->bajada; ?></p></a></li>
+						</ul>
+					<?php } ?>
+					</div>
+
+	
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 animate">
+					<div class="btn">
+						<div class="toggleIcon"></div>
+							<div class="ttl"><?php $notas =Notas::model()->findAllByAttributes(array('seccion'=>"rse"),"YEAR(fecha)=2007");?>2007</div>
+					</div>
+					<?php echo count($notas); 
+					foreach($notas as $nota){
+					?>
+						<ul>
+							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p><?php echo $nota->fecha; ?><div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse"><?php echo $nota->titulo; ?> </h1><p><?php echo $nota->bajada; ?></p></a></li>
+						</ul>
+					<?php } ?>
 					</div>
 					
 
@@ -153,38 +162,82 @@
 				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 animate">
 					<div class="btn">
 						<div class="toggleIcon"></div>
-							<div class="ttl">2004</div>
+							<div class="ttl"><?php $notas =Notas::model()->findAllByAttributes(array('seccion'=>"rse"),"YEAR(fecha)=2006");?>2006</div>
 					</div>
+					<?php echo count($notas); 
+					foreach($notas as $nota){
+					?>
 						<ul>
-							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p>07 de Octubre de 2004<div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse">3º Encuentro Biosolidario x Misiones Rurales Argentinas </h1></a></li>
-							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p>01 de Octubre de 2004<div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse">1º Encuentro Biosolidario de Uruguay </h1></a></li>
-							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p>05 de Junio de 2004<div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse">Encuentro Biosolidario Goya x Goya </h1></a></li>
-							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p>14 de Febrero de 2004<div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse">Encuentro Biosolidario del Fin del Mundo</h1></a></li>
+							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p><?php echo $nota->fecha; ?><div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse"><?php echo $nota->titulo; ?> </h1><p><?php echo $nota->bajada; ?></p></a></li>
 						</ul>
+					<?php } ?>
 					</div>
 					
+
+
 				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 animate">
 					<div class="btn">
 						<div class="toggleIcon"></div>
-							<div class="ttl">2003</div>
+							<div class="ttl"><?php $notas =Notas::model()->findAllByAttributes(array('seccion'=>"rse"),"YEAR(fecha)=2005");?>2005</div>
 					</div>
+					<?php echo count($notas); 
+					foreach($notas as $nota){
+					?>
 						<ul>
-							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p>11 de Septiembre de 2003<div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse">2º Encuentro Biosolidario x Misiones Rurales Argentinas </h1></a></li>
-							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p>05 de Julio de 2003<div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse">Encuentro Biosolidario Córdoba x Córdoba  </h1></a></li>
-							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p>12 de Abril de 2003<div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse">Encuentro Biosolidario Chubut x Chubut </h1></a></li>
+							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p><?php echo $nota->fecha; ?><div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse"><?php echo $nota->titulo; ?> </h1><p><?php echo $nota->bajada; ?></p></a></li>
 						</ul>
+					<?php } ?>
 					</div>
 					
+
+
+
 				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 animate">
 					<div class="btn">
 						<div class="toggleIcon"></div>
-							<div class="ttl">2002</div>
+							<div class="ttl"><?php $notas =Notas::model()->findAllByAttributes(array('seccion'=>"rse"),"YEAR(fecha)=2004");?>2004</div>
 					</div>
+					<?php echo count($notas); 
+					foreach($notas as $nota){
+					?>
 						<ul>
-							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p>05 de Septiembre de 2002<div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse">1º Encuentro Biosolidario x Misiones Rurales Argentinas </h1></a></li>
-							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p>13 de Julio de 2002<div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse">Encuentro Biosolidario Trenque Lauquen x Trenque Lauquen</h1></a></li>
-							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p>18 de Mayo de 2002<div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse">Encuentro Biosolidario Azul x Azul.  </h1></a></li>
+							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p><?php echo $nota->fecha; ?><div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse"><?php echo $nota->titulo; ?> </h1><p><?php echo $nota->bajada; ?></p></a></li>
 						</ul>
+					<?php } ?>
+					</div>
+					
+					
+					
+
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 animate">
+					<div class="btn">
+						<div class="toggleIcon"></div>
+							<div class="ttl"><?php $notas =Notas::model()->findAllByAttributes(array('seccion'=>"rse"),"YEAR(fecha)=2003");?>2003</div>
+					</div>
+					<?php echo count($notas); 
+					foreach($notas as $nota){
+					?>
+						<ul>
+							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p><?php echo $nota->fecha; ?><div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse"><?php echo $nota->titulo; ?> </h1><p><?php echo $nota->bajada; ?></p></a></li>
+						</ul>
+					<?php } ?>
+					</div>
+			
+
+			
+
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 animate">
+					<div class="btn">
+						<div class="toggleIcon"></div>
+							<div class="ttl"><?php $notas =Notas::model()->findAllByAttributes(array('seccion'=>"rse"),"YEAR(fecha)=2002");?>2002</div>
+					</div>
+					<?php echo count($notas); 
+					foreach($notas as $nota){
+					?>
+						<ul>
+							<li class="ripple" data-ripple-color="#38BFBF"><a hfref=""><p><?php echo $nota->fecha; ?><div class="linea-rse-not"></div></p><h1 class="h1-noticias-rse"><?php echo $nota->titulo; ?> </h1><p><?php echo $nota->bajada; ?></p></a></li>
+						</ul>
+					<?php } ?>
 					</div>
 					
 					
