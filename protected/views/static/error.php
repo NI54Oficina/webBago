f283cfa9e901b2b1038b5a51bcc045d8c02c1281
NI54-oficina <frantrf@gmail.com>
2016-03-21 14:52:29 -0300

    14:52 pm 21-03 Coni
    Pantalla de error

---
+++ protected/views/static/error.php
@@ -0,0 +1,32 @@
+<div id="container-error" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+	<div id="container-type-error" class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+		
+		<div id="container-error-title"><div class="point-error"></div><h1>ERROR</h1><div class="point-error"></div></div>
+
+		<div id="error-number">
+	
+			<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/numeros-error-01.png" alt="" />
+			<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/numeros-error-02.png" alt="" />
+			<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/numeros-error-01.png" alt="" />
+
+		</div>
+		
+	</div>
+	
+	<div id="container-text-error" class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+	
+		<h2>url que estaba buscando</h2>
+		
+		<div id="error-border"></div>
+		
+		<div id="error-description">
+		
+			<p>Descripción del error</p>
+		
+		</div>
+	
+	</div>
+	
+	
+</div>
