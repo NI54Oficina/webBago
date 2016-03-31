b3036002953e9b37e1b5e8190a0374da05954811
melmiranda <melania.smc@hotmail.com>
2016-03-31 11:04:51 -0300

    11:05 am 31/03/2016

--- protected/views/static/footer.php
+++ protected/views/static/footer.php
@@ -1,11 +1,6 @@
 	<footer class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 	
-	<img class="col-lg-12 col-md-12 col-sm-12 col-xs-12" src="<?php echo Yii::app()->request->baseUrl; ?>/img/curva-footer.png" />
-
-	<div class="redes-footer col-lg-12 col-md-12 col-sm-12 col-xs-12">
-	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 row" style="margin-bottom:3%; height: 50px;">
-
- <a href="https://www.facebook.com/BiogenesisBago/"> <img id="facebook" src="<?php echo Yii::app()->request->baseUrl; ?>/img/icono-fcbk.png" alt="" /></a>
+	<img class="col-lg-12 col-md-12 col-sm-12 col-xs-12" src="<?php echo Yii::app()->request->baseUrl; ?>/img/curva-footer.png" /><div class="redes-footer col-lg-12 col-md-12 col-sm-12 col-xs-12"><div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 row" style="margin-bottom:3%; height: 50px;"><a href="https://www.facebook.com/BiogenesisBago/"> <img id="facebook" src="<?php echo Yii::app()->request->baseUrl; ?>/img/icono-fcbk.png" alt="" /></a>
 	<!--<a href=""> <img id="gmail" src="<?php echo Yii::app()->request->baseUrl; ?>/img/icono-g.png" alt="" /></a>!-->
 	<a href="https://www.youtube.com/user/BiogenesisBago"> <img id="youtube" src="<?php echo Yii::app()->request->baseUrl; ?>/img/icono-ytb.png" alt="" /></a>
  </div> 
