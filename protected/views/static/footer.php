73dca7cda915e3c109306054b1b86592dc953300
melmiranda <melania.smc@hotmail.com>
2016-03-31 09:55:36 -0300

    21:55am 31/03/2016
    Correcciones footer

--- protected/views/static/footer.php
+++ protected/views/static/footer.php
@@ -1,7 +1,14 @@
 	<footer class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 	
 	<img class="col-lg-12 col-md-12 col-sm-12 col-xs-12" src="<?php echo Yii::app()->request->baseUrl; ?>/img/curva-footer.png" />
-	<div class="redes-footer col-lg-12 col-md-12 col-sm-12 col-xs-12"> <?php include('redes-sociales.php'); ?> </div>
+	<div class="redes-footer col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+ <a href="https://www.facebook.com/BiogenesisBago/"> <img id="facebook" src="<?php echo Yii::app()->request->baseUrl; ?>/img/icono-fcbk.png" alt="" /></a>
+	<!--<a href=""> <img id="gmail" src="<?php echo Yii::app()->request->baseUrl; ?>/img/icono-g.png" alt="" /></a>!-->
+	<a href="https://www.youtube.com/user/BiogenesisBago"> <img id="youtube" src="<?php echo Yii::app()->request->baseUrl; ?>/img/icono-ytb.png" alt="" /></a>
+ </div> 
+
+ </div>
 	<div class="copyright col-lg-12 col-md-12 col-sm-12 col-xs-12"><p style="display: inline-block;text-align: center;margin: auto;">Copyright de Biogénesis Bagó</p>    
 	<a href="#" role="button"><img id="btnmenu" src="<?php echo Yii::app()->request->baseUrl; ?>/img/flecha-menu-arriba.png" style="margin-top:0%;"></a>
 	</div>
