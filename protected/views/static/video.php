9e5ae64177065e61c5d538933f75c6f23376b1cc
NI54-oficina <frantrf@gmail.com>
2016-03-31 15:17:03 -0300

    15:16 pm 31-03 Coni

--- protected/views/static/video.php
+++ protected/views/static/video.php
@@ -17,36 +17,47 @@
 	    });
     </script>
 	<div >
-	<div class=" col-lg-12 col-md-12 col-sm-12 col-xs-12 redes-home-video">
+	
+	<div class=" col-lg-12 col-md-12 col-sm-12 col-xs-12">
+	
+	<div class=" col-lg-6 col-md-6 col-sm-6 col-xs-6" >
+	<!-- TEXTO -->
+	<div class="general-texto-home-header" >
+		<h1  style="margin: 0; margin-top: 7%; text-align: left; margin-left: 60px;" class="h1-home-ev bienvenidos"><?php echo Textos::model()->GetText(1); ?></h1>
+	</div>
+	
+	
+	</div>
+	
+	<div class=" col-lg-6 col-md-6 col-sm-6 col-xs-6 redes-home-video">
 		<!--<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/pais/traduccion/id/1/lng/0"style="color:white;float:left;padding:10px;opacity:0.8;">Español</a>
 		<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/pais/traduccion/id/2/lng/2"style="color:white;float:left;padding:10px;opacity:0.8;">Portugues</a>!-->
 		<?php include('redes-sociales-home-header.php'); ?> 
 	
 	</div>
-	<div style="min-height:100vh;" align="center">
-	<!-- TEXTO -->
-	<div class=" col-lg-12 col-md-12 col-sm-12 col-xs-12 general-texto-home-header" >
-		<h1  class="h1-home-ev bienvenidos"><?php echo Textos::model()->GetText(1); ?></h1>
-		<img style="margin-top: 5%; margin-right: 1%"src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo-blanco.png" alt="logo-bago"  class="logo-blanco" />
-		<div  class="border-video"></div>
+	</div>
+	
+	
+		
+		<div class=" col-lg-12 col-md-12 col-sm-12 col-xs-12" style="margin:auto; text-align:center" align="center"><img style="margin-right: 1%"src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo-blanco.png" alt="logo-bago"  class="logo-blanco" />		<div  class="border-video"></div></div>
+
 
 		<div class="content-home">
 			<h1 class="h1-home-ev row" style="text-transform: uppercase;"><?php echo Textos::model()->GetText(2); ?></h1>
 			<h1 class="h1-home-ev salud-animal row" style="text-transform: uppercase;"><?php echo Textos::model()->GetText(3); ?></h1>
 		</div>
-	</div>
-	
-	</div>
+		<p class="scroll-down"><a href="#complements" class="animate">More</a></p>
 	<!-- TEXTO  PRIMERA SECCION-->
-	<div class="general-texto-home-header div-inst">
+	<div class="general-texto-home-header div-inst" style="padding-top:380px;">
 
 		<h1  class="h1-inst-home"><img class="img-punto-home" src="<?php echo Yii::app()->request->baseUrl; ?>/img/punto-home.png"  style="    vertical-align: top;margin-right:8px;text-transform: uppercase;"/><?php echo Textos::model()->GetText(347); ?></h1>
 		<img   class="img-punto-home" src="<?php echo Yii::app()->getBaseUrl(true); ?>/img/punto-home.png"  /><br>
 		<div class="border-inst"></div><br>
 		<p  class="p-inst-home"><?php echo Textos::model()->GetText(4); ?> <br /> <?php echo Textos::model()->GetText(5); ?></p>
 	</div>
-	</div>
 	
+
+	</div>
 	<div class="overlay-video">
 	
 	</div>
