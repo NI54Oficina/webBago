38bcf6ca8695f7fa7c2677e06cd3afe8125999e1
Fran <fran@ni54.com>
2016-04-05 18:06:38 -0300

    05-04 18:06
    z-index nuestras plantas

--- protected/views/static/nuestras-plantas.php
+++ protected/views/static/nuestras-plantas.php
@@ -22,7 +22,7 @@
 	<div class="div-texto-aftosa col-lg-6 col-md-6 col-sm-12 col-xs-12" style="text-align: left;">
 		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align:right;" id="nuestrasPlantasTopVideo">
 		
-		<a href="nuestras-plantas-menu" style="display:inline-block;float:right;width:150%;height:50vh;position:relative;z-index:10000;" id="videoPlanta">
+		<a href="nuestras-plantas-menu" style="display:inline-block;float:right;width:150%;height:50vh;position:relative;" id="videoPlanta">
 			<iframe width="100%" height="100%" allowfullscreen style="border-style:none;" src="<?php echo Yii::app()->request->baseUrl; ?>/pannellum-master/src/standalone/pannellum.htm?config=../../examples/exterior.json" ></iframe>
 			<img style="margin-top: -10%;margin-left: 80%;position:relative;z-index:100;" src="<?php echo Yii::app()->request->baseUrl; ?>/img/mas.png" alt="" />
 			<!--<object id="videoPlantaVideo" width="100%" height="500" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" >
