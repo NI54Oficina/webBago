30904068eaf2cbd7a4b62ed1103cdd4857125e56
Fran <fran@ni54.com>
2016-04-07 19:25:49 -0300

    07-05 19:25

--- protected/views/static/footer-home.php
+++ protected/views/static/footer-home.php
@@ -1,4 +1,4 @@
                <div class="copy col-lg-12 col-md-12 col-sm-12 col-xs-12" style="height:auto;width:100%;position:relative;bottom:-5px;">
                <p style="text-align:center; padding-top:1%;"><?php echo Textos::model()->GetText(489); ?></p>
-                 <a href="#" role="button"><img id="btnmenu" src="<?php echo Yii::app()->getBaseUrl(true); ?>/img/flecha-menu-arriba.png"></a>
+                 <a href="#" role="button"  target="_blank"><img id="btnmenu" src="<?php echo Yii::app()->getBaseUrl(true); ?>/img/flecha-menu-arriba.png"></a>
                </div>
--- protected/views/static/redes-sociales-home-pc.php
+++ protected/views/static/redes-sociales-home-pc.php
@@ -1,6 +1,6 @@
 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 row" class="hidden-sm hidden-xs" style="margin:auto;margin-top:2%; height: 50px; background: transparent">
-	<a href="https://www.facebook.com/BiogenesisBago/"> <img id="facebook" src="<?php echo Yii::app()->getBaseUrl(true); ?>/img/icono-fcbk.png" alt="" /></a>
+	<a href="https://www.facebook.com/BiogenesisBago/" target="_blank"> <img id="facebook" src="<?php echo Yii::app()->getBaseUrl(true); ?>/img/icono-fcbk.png" alt="" /></a>
 	<!--<a href=""> <img id="gmail" src="img/icono-g.png" alt="" /></a>!-->
-	<a href="https://www.youtube.com/user/BiogenesisBago"> <img id="youtube" src="<?php echo Yii::app()->getBaseUrl(true); ?>/img/icono-ytb.png" alt="" /></a>
+	<a href="https://www.youtube.com/user/BiogenesisBago"  target="_blank"> <img id="youtube" src="<?php echo Yii::app()->getBaseUrl(true); ?>/img/icono-ytb.png" alt="" /></a>
 </div>
 
