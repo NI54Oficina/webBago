4070686240d8c407ab960aab5759c4a08f5c9e41
Fran <fran@ni54.com>
2016-04-12 10:19:39 -0300

    12-04 10:19

--- protected/views/static/paises.php
+++ protected/views/static/paises.php
@@ -14,22 +14,78 @@
    <!-- BigVideo Dependencies -->
     
 	
-	<div class="overlay-video" style="z-index:0;">
 	
-	</div>
 	<div style="width:100vw; min-height:100vh; z-index:10; position:relative;"> 
     <!-- BigVideo -->
     
 
     <!-- Demo -->
 
-     <script>
+     <!--<script>
 	    $(function() {
             var BV = new $.BigVideo();
 			BV.init();
 			BV.show('<?php echo Yii::app()->getBaseUrl(true); ?>/video/home1.mp4',{ambient:true});
 	    });
-    </script>
+    </script>!-->
+	
+	<script>
+	var vid = document.getElementById("bgvid");
+var pauseButton = document.querySelector("#polina button");
+
+function vidFade() {
+  vid.classList.add("stopfade");
+}
+
+vid.addEventListener('ended', function()
+{
+// only functional if "loop" is removed 
+vid.pause();
+// to capture IE10
+vidFade();
+}); 
+
+
+pauseButton.addEventListener("click", function() {
+  vid.classList.toggle("stopfade");
+  if (vid.paused) {
+    vid.play();
+    pauseButton.innerHTML = "Pause";
+  } else {
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+  }
+})
+
+	</script>
+	
+	<style>
+	video { 
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    min-width: 100%;
+    min-height: 100%;
+    width: auto;
+    height: auto;
+    
+    transform: translateX(-50%) translateY(-50%);
+ /*background: url('//demosthenes.info/assets/images/polina.jpg') no-repeat;*/
+  background-size: cover;
+  transition: 1s opacity;
+}
+	</style>
+	
+	
+	<video autoplay  poster="<?php echo Yii::app()->getBaseUrl(true); ?>/video/home1.mp4" id="bgvid" loop>
+  <!-- WCAG general accessibility recommendation is that media such as background video play through only once. Loop turned on for the purposes of illustration; if removed, the end of the video will fade in the same way created by pressing the "Pause" button  -->
+<!--<source src="<?php echo Yii::app()->getBaseUrl(true); ?>/video/home1.mp4" type="video/webm">!-->
+<source src="<?php echo Yii::app()->getBaseUrl(true); ?>/video/home1.mp4" type="video/mp4">
+</video>
+
+<div class="overlay-video" style="z-index:0;">
+	
+	</div>
 
 	<!-- PAÍSES -->
 	<div id="epigrafe-mobile"  class="col-lg-12 col-md-12 col-sm-12 col-xs-12"><img  src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo-blanco.png" style=""/></div>
