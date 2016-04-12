b30c443e2d5ec59a149f7ade4b17e2a71b0c0d17
melmiranda <melania.smc@hotmail.com>
2016-04-12 11:50:59 -0300

    11:50  12/04/2016

--- protected/views/static/redes-sociales-home-header.php
+++ protected/views/static/redes-sociales-home-header.php
@@ -1,4 +1,4 @@
-<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 row" style="width: 100%; margin-top: 5%">
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 row redessociales-mobile" style="width: 100%; margin-top: 5%">
 	<?php if($_SESSION["lng"]=="pt"){ ?>
 	<a id="fb-home" style=" margin-right: 1%" href="https://www.facebook.com/biogenesisbagobr/"  target="_blank"> <img id="" src="<?php echo Yii::app()->request->baseUrl; ?>/img/icono-fcbk.png" alt="" /></a>
 	<?php }else{ ?>
