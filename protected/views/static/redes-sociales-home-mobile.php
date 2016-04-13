8b64d19e6cb51fcae5a460dc3f77f7621f7f71e2
melmiranda <melania.smc@hotmail.com>
2016-04-13 15:21:41 -0300

    15:20 13-04-2016

--- protected/views/static/redes-sociales-home-mobile.php
+++ protected/views/static/redes-sociales-home-mobile.php
@@ -1,4 +1,4 @@
-<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 row hidden-lg hidden-md" style="padding-top: 10%;padding-left: 3%;">
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 row hidden-lg hidden-md" style="padding-top: 10%;padding-left: 3%; margin-right: auto; margin-left: auto;">
 	<?php if($_SESSION["lng"]=="pt"){ ?>
 	<a href="https://www.facebook.com/biogenesisbagobr/"> <img id="facebook" src="<?php echo Yii::app()->getBaseUrl(true); ?>/img/icono-fcbk.png" alt="" /></a>
 	<?php }else{ ?>
